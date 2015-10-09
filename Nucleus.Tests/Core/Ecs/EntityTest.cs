f0ab5f8d9a8f6ea1616d3a0d5f5de8713ebb947d
ashes999 <ashes999@yahoo.com>
2015-10-09 18:05:45 -0400

    Moved unit tests into Nucleus repository

---
+++ Nucleus.Tests/Core/Ecs/EntityTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Nucleus.Ecs;
+
+namespace Nucleus.Tests
+{
+    [TestFixture]
+    public class EntityTest
+    {
+        [Test]
+        public void GetGetsSetValue()
+        {
+            var e = new Entity();
+            e.Add(new StringComponent("a")).Add(new StringComponent("b"));
+            var actual = e.Get<StringComponent>();
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual.Value, Is.EqualTo("b"));
+        }
+
+        [Test]
+        public void GetReturnsEmptyListIfNoComponentsAreSet()
+        {
+            var e = new Entity();
+            var ex = Assert.Throws<ArgumentException>(() => e.Get<StringComponent>());
+        }
+
+
+        class StringComponent : Component
+        {
+            public string Value { get;set; }
+
+            public StringComponent(string value) {
+                this.Value = value;
+            }
+
+        }
+    }
+}
+
